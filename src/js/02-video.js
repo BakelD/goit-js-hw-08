8785b2f61c371b5ff0db05406402cc39ee78f562
BakelD <bakeld85@gmail.com>
2022-05-27 16:35:54 +0300

    Done task 2

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,12 @@
+import throttle from 'lodash.throttle';
 
+const iframeEl = document.querySelector('iframe');
+const player = new Vimeo.Player(iframeEl);
+
+const onTimeUpdate = ({ seconds }) => {
+  localStorage.setItem('videoplayer-current-time', seconds);
+};
+player.on('timeupdate', throttle(onTimeUpdate, 1000));
+
+const seconds = localStorage.getItem('videoplayer-current-time');
+player.setCurrentTime(seconds);
