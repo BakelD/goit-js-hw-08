3c071e97dc48bfb1634274360bc212ae53622261
BakelD <bakeld85@gmail.com>
2022-06-02 22:54:26 +0300

    Fixed bug with video task

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -3,7 +3,10 @@ import throttle from 'lodash.throttle';
 const iframeEl = document.querySelector('iframe');
 const player = new Vimeo.Player(iframeEl);
 
-localStorage.setItem('videoplayer-current-time', 0);
+const initTime = localStorage.getItem('videoplayer-current-time');
+if (initTime === null) {
+  localStorage.setItem('videoplayer-current-time', 0);
+}
 
 const onTimeUpdate = ({ seconds }) => {
   localStorage.setItem('videoplayer-current-time', seconds);
